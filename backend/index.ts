10ea7763123afb5ad13e44c4400dd60d6e018fe4
sweeneyb <tab126@gmail.com>
2023-07-29 18:52:54 -0400

    have a couple of things coming out of the API

--- backend/index.ts
+++ backend/index.ts
@@ -1,6 +1,8 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 
+import {Tenant} from './types'
+
 dotenv.config();
 
 const app: Express = express();
@@ -8,6 +10,20 @@ const port = process.env.PORT;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server!');
+});
+
+app.get('/tenants', (req: Request, res: Response) => {
+  let tenants = new Set<Tenant>()
+  tenants.add(new Tenant("T1"))
+  tenants.add(new Tenant("T2"))
+  // console.log(tenants.size)
+  res.send(JSON.stringify(Array.from(tenants)));
+});
+
+
+app.get('/tenant/:tenantId', (req: Request, res: Response) => {
+  let tenant : Tenant =  new Tenant(req.params.tenantId)
+  res.send(tenant);
 });
 
 app.listen(port, () => {
---
+++ backend/types.ts
@@ -0,0 +1,10 @@
+
+
+export class Tenant  {
+    public name: string;
+    public size: number = 42
+
+    constructor(name: string) {
+        this.name = name
+    }
+}
